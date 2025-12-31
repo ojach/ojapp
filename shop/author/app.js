67e044290ecf7f67082d13dc2ee0f2398580e8e8
ojach <trc.wasps@gmail.com>
2025-12-31 10:21:55 +0900

    Update app.js

--- shop/author/app.js
+++ shop/author/app.js
@@ -27,7 +27,7 @@ const base = "/shop/author/sns-icon/";
   { key: "sns_insta", url: data.sns_insta, file: "instagram-icon.svg" },
     { key: "sns_threads", url: data.sns_threads, file: "threads.svg" },
     { key: "sns_booth",   url: data.sns_booth,   fije: "pixiv_white.svg" },
-    { key: "sns_site",    url: data.sns_site,    svg: `<path d="M12 5l6 14H6z"/>` }
+    { key: "sns_site",    url: data.sns_site,    fije: "site.svg" }
   ];
   
 
