b6a624835cd41c317c20d6df6eb345919b995461
ojach <trc.wasps@gmail.com>
2026-01-03 09:32:45 +0900

    Update sw.js

--- kachi-plus/sw.js
+++ kachi-plus/sw.js
@@ -1,1 +1,48 @@
+const CACHE_NAME = "kachi-plus-v3";
+
+const urlsToCache = [
+  "/kachi-plus/",
+  "/kachi-plus/index.html",
+  "/kachi-plus/free.html",
+  "/kachi-plus/juggler.html",
+  "/kachi-plus/style.css",
+  "/kachi-plus/sw.js",
+  "/icon/icon-180.png",
+  "/icon/ojapp-logo.png",
+  "/icon/favicon-16.png",
+  "/icon/favicon-32.png"
+];
+
+// install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
+    })
+  );
+});
 
